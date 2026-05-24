f6d5cd53a310166599f04cf42728f92e9cb597de
Jaeden Doody <subscriptions@stillawakemedia.com>
2026-05-24 02:15:17 -0400

    Fix generated robots policy

--- src/app/robots.js
+++ src/app/robots.js
@@ -1,24 +1,11 @@
 export default function robots() {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-      disallow: [
-        "/wp-admin/",
-        "/administrator",
-        "/image/",
-        "/wp-content/",
-        "/wp-includes/",
-        "/category/",
-        "/tag/",
-        "/author/",
-        "/slottica-casino/",
-        "/slot-gacor-online.jpg",
-        "/2019/",
-        "/2020/",
-      ],
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
     sitemap: "https://bankdemark.com/sitemap.xml",
-    host: "https://bankdemark.com",
   };
 }
